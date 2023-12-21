c5697779d1017d29414b1101031bcd9c62dc0086
ChiaChi Tsai <rueshyna@gmail.com>
2023-12-21 13:02:55 +0800

    debug: ci

--- chart/src/App.tsx
+++ chart/src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Sidebar from './component/Sidebar';
 import Chart from './component/Chart';
 import SortableTable from './component/SortableTable';
 
 const App: React.FC = (): JSX.Element => {
   return (
-    <Router>
-      <div className="flex">
-        <Sidebar />
-        <div className="flex-grow">
-          <Routes>
-            <Route path="/stats" element={<Chart />} />
-            <Route path="/safes" element={<SortableTable />} />
-            <Route path="/" element={<Chart />} />
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Routes>
+      <Router>
+        <div className="flex">
+          <Sidebar />
+          <div className="flex-grow">
+            <Routes>
+              <Route path="/stats" element={<Chart />} />
+              <Route path="/safes" element={<SortableTable />} />
+              <Route path="/" element={<Chart />} />
+            </Routes>
+          </div>
         </div>
-      </div>
-    </Router>
+      </Router>
   );
 };
 
